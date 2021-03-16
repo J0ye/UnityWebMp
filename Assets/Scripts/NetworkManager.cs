f747c42c03458650fa235baaf466f07d6d0956c0
J0ye <jonathan.mueller@fh-erfurt.de>
2021-03-16 15:45:43 +0100

    Added getplayers and ProcessMessage for player list

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -11,6 +11,9 @@ using HybridWebSocket;
 public class NetworkManager : MonoBehaviour
 {
     public PlayerController player;
+    [Tooltip ("Determines how often information is sent to the server, in seconds.")]
+    public float pingFrequency = 1f;
+    public bool debug = false;
 
     private WebSocket2DMp webSocket;
     private bool readyForId = false;
@@ -19,6 +22,7 @@ public class NetworkManager : MonoBehaviour
     {
         StartCoroutine(SetUpSocket());
         StartCoroutine(SendPlayerPos());
+        StartCoroutine(GetPlayers());
     }
 
     private IEnumerator SetUpSocket()
@@ -47,20 +51,42 @@ public class NetworkManager : MonoBehaviour
             float x = player.transform.position.x;
             float y = player.transform.position.y;
             string msg = "Pos:" + x + "/" + y + "/" + player.GetId();
-            Debug.Log("Sending " + msg);
+            if(debug) Debug.Log("Sending " + msg);
             webSocket.Send(msg);
         }
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(pingFrequency);
         StartCoroutine(SendPlayerPos());
+    }
+
+    private IEnumerator GetPlayers()
+    {
+        if (webSocket != null)
+        {
+            webSocket.Send("Get:" + player.GetId());
+        }
+        yield return new WaitForSeconds(pingFrequency);
+        StartCoroutine(GetPlayers());
     }
 
     private void ProcessMessage(string msg)
     {
+        // If the message is about the players new ID
         if(readyForId && msg.Contains("ID"))
         {
             var parts = msg.Split(": ".ToCharArray());
             Guid newId = Guid.Parse(parts[2]);
             player.SetId(newId);
+        }
+        // If the message is about the list of players
+        else if(msg.Contains("Players:"))
+        {
+            var parts = msg.Split(":".ToCharArray());
+            parts = parts[1].Split("%".ToCharArray());
+            if(debug) Debug.Log("Parts Count: " + parts.Length);
+            if(parts.Length > 1)
+            {
+                if(debug) Debug.Log("player 0: " + parts[0] + " and Player 1: " + parts[1]);
+            }
         }
     }
 }
